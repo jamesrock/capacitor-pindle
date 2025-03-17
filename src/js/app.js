e9a792a270431a4592b6ed72a007fdc2ae7eedab
James Rock <hello@jamesrock.me>
2025-03-17 16:42:41 +0000

    updates

--- src/js/app.js
+++ src/js/app.js
@@ -3,30 +3,9 @@ import { ROCK_MATH } from './ROCK.js';
 class Pindle {
   constructor() {
 
-    this.pins = this.makePins();
-    this.selection = this.makeSelection();
-    this.attempts = this.makeAttempts();
-    this.buttons = this.makeButtons();
+    this.reset();
 
   };
-  colors = [
-    'color0',
-    'color1',
-    'color2',
-    'color3',
-    'color4',
-    'color5',
-    'color6',
-    'color7',
-    'color8',
-    'color9'
-  ];
-  numberOfPinsInCode = 5;
-  numberOfEachColor = 5;
-  numberOfAttempts = 6;
-  attemptIndex = 0;
-  attemptPinIndex = 0;
-  solved = false;
   makePins() {
 
     const out = [];
@@ -104,20 +83,50 @@ class Pindle {
         })[0].setState('disabled');
       };
     });
-    if(correct===5) {
+    if(correct===this.numberOfPinsInCode) {
       this.solved = true;
     };
+    return this;
   };
+  checkGaneOver() {
+    return !this.solved && this.attemptIndex > (this.attempts.length - 1);
+  };
+  reset() {
+    
+    this.pins = this.makePins();
+    this.selection = this.makeSelection();
+    this.attempts = this.makeAttempts();
+    this.buttons = this.makeButtons();
+    this.solved = false;
+    this.attemptIndex = 0;
+    this.attemptPinIndex = 0;
+    return this;
+
+  };
+  colors = [
+    'color0',
+    'color1',
+    'color2',
+    'color3',
+    'color4',
+    'color5',
+    'color6',
+    'color7',
+    'color8',
+    'color9'
+  ];
+  numberOfPinsInCode = 5;
+  numberOfEachColor = 5;
+  numberOfAttempts = 6;
+  attemptIndex = 0;
+  attemptPinIndex = 0;
+  solved = false;
 };
 
 class Pin {
-  constructor(
-    color
-  ) {
+  constructor(color) {
     this.color = color;
   };
-  color = 'empty';
-  state = 'pin';
   setState(state) {
     this.state = state;
     return this;
@@ -126,6 +135,8 @@ class Pin {
     this.color = color;
     return this;
   };
+  color = 'empty';
+  state = 'pin';
 };
 
 const root = document.querySelector(':root');
@@ -143,13 +154,20 @@ const makeAttempt = (attempt) => {
 };
 
 const render = () => {
+  const gameover = pindle.solved || pindle.checkGaneOver();
   let markup = `<div class="board">`;
   markup += `<div class="selection" data-solved=${pindle.solved}>${pindle.selection.map((pin) => {return `<div class="pin selected ${pin.color}"></div>`}).join('')}</div>`;
-  markup += `<div class="attempts">${pindle.attempts.map((attempt) => {return `<div class="attempt">${makeAttempt(attempt)}</div>`}).join('')}</div>`;
-  markup += `<div class="controls">`;
-  markup += `<div class="buttons" style="width: ${(size*5)+(gap*4)}px">${pindle.buttons.map((pin) => {return `<div data-color="${pin.color}" data-state="${pin.state}" class="pin button ${pin.color}"></div>`}).join('')}</div>`;
-  markup += `<div class="foot">pindle</div>`;
+  markup += `<div class="attempts" data-gameover=${gameover}>${pindle.attempts.map((attempt) => {return `<div class="attempt">${makeAttempt(attempt)}</div>`}).join('')}</div>`;
+  markup += `<div class="controls" data-gameover=${gameover}>`;
+    markup += `<div class="buttons" style="width: ${(size*5)+(gap*4)}px">${pindle.buttons.map((pin) => {return `<div data-color="${pin.color}" data-state="${pin.state}" class="pin button ${pin.color}"></div>`}).join('')}</div>`;
+    markup += `<div class="foot">pindle</div>`;
   markup += `</div>`;
+  if(pindle.checkGaneOver()) {
+    markup += `<div class="game-over"><div class="game-over-body"><h2>GAME OVER</h2><p>Press to try again.</p></div></div>`;
+  };
+  if(pindle.solved) {
+    markup += `<div class="game-over"><div class="game-over-body"><h2>Well done!</h2><p>You solved the puzzle in ${pindle.attemptIndex} tries!</p><p>Press to try again.</p></div></div>`;
+  };
   markup += `</div>`;
   document.body.innerHTML = markup;
 };
@@ -159,6 +177,10 @@ render();
 document.addEventListener('click', (e) => {
   if(e.target.classList.contains('button')) {
     pindle.addAttempt(e.target.getAttribute('data-color'));
+    render();
+  };
+  if(e.target.classList.contains('game-over')) {
+    pindle.reset();
     render();
   };
 });
